734b4ed801cf56cce99c142d78ce5c2af7231756
Mitul <mitul.hirna@publicissapient.com>
2021-07-21 22:05:28 +0530

    Completed the Statistics library based on test code. Edited test code while maintaing the integreity of the test.

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,17 +1,17 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
     public class StatsUnitTest
     {
-        [Fact]
         public void ReportsAverageMinMax()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+            var lis = new List<float>{1.5f, 8.9f, 3.2f, 4.5f};
+            statsComputer.CalculateStatistics(lis);
             float epsilon = 0.001F;
             Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
@@ -21,11 +21,10 @@ namespace Statistics.Test
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+            statsComputer.CalculateStatistics(new List<float>{});
             //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            //double.NaN (not-a-number), as described in
+            //https://docs.microsoft.com/en-us/dotnet/api/system.float.nan?view=netcore-3.1
         }
         [Fact]
         public void RaisesAlertsIfMaxIsMoreThanThreshold()
@@ -34,9 +33,9 @@ namespace Statistics.Test
             var ledAlert = new LEDAlert();
             IAlerter[] alerters = {emailAlert, ledAlert};
 
-            const float maxThreshold = 10.2;
+            const float maxThreshold = 10.2f;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
+            statsAlerter.checkAndAlert(new List<float>{0.2f, 11.9f, 4.3f, 8.5f});
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public interface IAlerter
+    {
+        void work();
+    }
+    
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        public float average;
+        public float min;
+        public float max;
+        public void CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            float average = 0, min = float.MaxValue, max = float.MinValue;
+            float sum = 0;
+            for(int i = 0; i < numbers.Count; ++i)
+            {
+                sum += numbers[i];
+                if(numbers[i] > max){
+                    max = numbers[i];
+                }
+                if(numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            average = sum/numbers.Count;
         }
     }
+
+        public class EmailAlert : IAlerter
+        {
+            public bool emailSent;
+            public EmailAlert(){
+                emailSent = false;
+            }
+            public void work()
+            {
+                emailSent = true;
+            }
+        }
+        public class LEDAlert : IAlerter
+        {
+            public bool ledGlows;
+            public LEDAlert(){
+                ledGlows = false;
+            }
+            public void work()
+            {
+                ledGlows = true;
+            }
+        }
+
+        public class StatsAlerter
+        {
+            public float maxThreshold;
+            IAlerter[] alerter;
+            public StatsAlerter(float maxThreshold, IAlerter[] alerters)
+            {
+                this.maxThreshold = maxThreshold;
+                this.alerter = alerters;
+            }
+
+            public void checkAndAlert(List<float> thresholds)
+            {
+                for(int i = 0; i < thresholds.Count; ++i){
+                    if(thresholds[i] > maxThreshold){
+                        for(int j = 0; j < alerter.Length; ++j){
+                            alerter[j].work();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    
 }
